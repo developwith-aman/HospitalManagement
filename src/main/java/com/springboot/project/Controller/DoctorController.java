bfee0e365e6c5b6a5c880656a8eedcf1b3725381
Aman Singh <developwithaman@gmail.com>
2025-09-09 16:31:06 +0530

    Move Doctor functions in DoctorController

---
+++ src/main/java/com/springboot/project/Controller/DoctorController.java
@@ -0,0 +1,33 @@
+package com.springboot.project.Controller;
+
+import com.springboot.project.dto.AddDoctor;
+import com.springboot.project.entity.Appointment;
+import com.springboot.project.entity.Doctor;
+import com.springboot.project.service.AppointmentService;
+import com.springboot.project.service.DoctorService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/doctors")
+public class DoctorController {
+    private final DoctorService doctorService;
+    private final AppointmentService appointmentService;
+
+    public DoctorController(DoctorService doctorService,
+                            AppointmentService appointmentService) {
+        this.doctorService = doctorService;
+        this.appointmentService = appointmentService;
+    }
+
+    @PostMapping(value = "/add/doctor")
+    public Doctor addDoctor(@RequestBody AddDoctor addDoctor) {
+        return doctorService.addDoctor(addDoctor);
+    }
+
+    @PatchMapping(value = "/assign/new/doctor")
+    public Appointment reassignAppointmentToAnotherDoctor(
+            @RequestParam int doctorID,
+            @RequestParam Long appointmentID) {
+        return appointmentService.reassignAppointment(doctorID, appointmentID);
+    }
+}
